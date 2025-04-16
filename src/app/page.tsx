c3c290f398cc71378b5db57166b06e4df2544673
sawnedcom <kadek.nobel@gmail.com>
2025-04-16 23:23:18 +0800

    I THINK NOW IS DONE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/pages/navbar";
 import HomeSection from "./components/pages/home";
 import Properties from "./components/pages/properties";
-import Aboutus from "./components/pages/aboutUs";
+import AboUtus from "./components/pages/aboutUs";
 import Selingan from "./components/pages/selingan";
 import OurtTeam from "./components/pages/ourTeam";
 import ContactUs from "./components/pages/contactUs";
@@ -13,7 +13,7 @@ export default function Home() {
       <Navbar />
       <HomeSection />
       <Properties />
-      <Aboutus />
+      <AboUtus />
       <Selingan />
       <OurtTeam />
       <ContactUs />
